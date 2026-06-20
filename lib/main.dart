03c48b3d65f5caccc35ae388f537fcad5e3bca03
ChandruBtechCSE <karegoudracm@gmail.com>
2026-06-20 23:39:35 +0530

    updated .gitignore and untracked few files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:iiitk_events/pages/login_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: LoginPage(),);
   }
 }
 
